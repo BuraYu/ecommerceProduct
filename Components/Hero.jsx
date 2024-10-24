2a810e6ca30660a8049dfed82f033acd8eee9dea
BuraYu <burak.yueksel@hotmail.com>
2024-10-24 19:20:12 +0200

    add css, theme for mui

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -1,10 +1,16 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <Box>
+      <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h6">Heading 6</Typography>
+      <Typography variant="overline">Overline</Typography>
+      <Typography variant="subtitle1">Subtitle</Typography>
+      <Typography variant="body1">Body Text</Typography>
+    </Box>
   );
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,60 @@
 import "../styles/main.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontWeight: "bold",
+      fontSize: "3.5rem",
+      textTransform: "uppercase",
+      lineHeight: "3.625rem",
+      letterSpacing: "0.125em",
+    },
+    h2: {
+      fontWeight: "bold",
+      fontSize: "2.5rem",
+      textTransform: "uppercase",
+      lineHeight: "2.75rem",
+      letterSpacing: "0.09375rem",
+    },
+    h6: {
+      fontWeight: "bold",
+      fontSize: "1.125rem",
+      textTransform: "uppercase",
+      lineHeight: "1.5rem",
+      letterSpacing: "0.08125rem",
+    },
+    overline: {
+      fontWeight: "normal",
+      fontSize: "0.875rem",
+      textTransform: "uppercase",
+      lineHeight: "1.1875rem",
+      letterSpacing: "0.625rem",
+    },
+    subtitle1: {
+      fontWeight: "bold",
+      fontSize: "0.8125rem",
+      textTransform: "uppercase",
+      lineHeight: "1.5625rem",
+      letterSpacing: "0.0625rem",
+    },
+    body1: {
+      fontWeight: 500,
+      fontSize: "0.9375rem",
+      lineHeight: "1.5625rem",
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
